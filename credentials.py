456609a3a5d6cd92929f8623674cbd6f697554c7
Derek Caelin <derek.caelin@gmail.com>
2017-09-12 10:43:34 -0400

    Delete credentials.py

--- credentials.py
+++
@@ -1,5 +0,0 @@
-#encoded login info
-headers = {
-    'authorization': "Basic TTl2cE8waXVacmFwVHVIa2RYRzB1aXJBbkNweWFtUng6TXN4endWbExNOVQ4RTJnRg==",
-    'content-type': "application/x-www-form-urlencoded"
-    }
